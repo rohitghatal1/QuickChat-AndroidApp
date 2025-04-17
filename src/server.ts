5d0d12e85e334aa1d2c095792738fe325dbc208a
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-04-17 20:18:20 +0545

    updated server with socket connection

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,45 @@ import authRoutes from "./routes/authRoutes";
 import errorHandler from "./utils/errorHandler"
 import userRoutes from "./routes/userRoutes";
 import amdinAuth from "./routes/admin/authRoutesAdmin";
+import {Server as SocketIOSever} from "socket.io";
+import http from "http";
 
 dotenv.config();
 
 const app = express();
+var server = http.createServer(app);
 
+//Socket.IO server initialization
+var io = new SocketIOSever(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCh", "DELETE"]
+    }
+})
+
+//middlewares
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(errorHandler)
 
+//routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes );
 app.use("/api/admin", amdinAuth )
 
+//Global error handler
+app.use(errorHandler);
+
+//Socket.IO connection listener
+io.on("connection", (socket:any) => {
+    console.log("new client connected: ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected: ", socket.id);
+    })
+})
+
+// start the server
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
